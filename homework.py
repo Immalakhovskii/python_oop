7756082348f15c8b6f22a160565c547812e3e1ac
Sasha Malakhovskii <sky4place@gmail.com>
2022-01-20 07:13:20 +0300

    Homework fitness module V1.2.2

--- homework.py
+++ homework.py
@@ -6,25 +6,25 @@ from typing import ClassVar, Dict, List
 class InfoMessage:
     """Информационное сообщение о тренировке."""
     training_type: str
-    duration_h: float
-    distance_km: float
-    speed_km_h: float
-    calories_kcal: float
+    duration: float
+    distance: float
+    speed: float
+    calories: float
 
     def get_message(self) -> str:
         return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {"{:.3f}".format(self.duration_h)} ч.; '
-                f'Дистанция: {"{:.3f}".format(self.distance_km)} км; '
-                f'Ср. скорость: {"{:.3f}".format(self.speed_km_h)} км/ч; '
-                f'Потрачено ккал: {"{:.3f}".format(self.calories_kcal)}.')
+                f'Длительность: {"{:.3f}".format(self.duration)} ч.; '
+                f'Дистанция: {"{:.3f}".format(self.distance)} км; '
+                f'Ср. скорость: {"{:.3f}".format(self.speed)} км/ч; '
+                f'Потрачено ккал: {"{:.3f}".format(self.calories)}.')
 
 
 @dataclass
 class Training:
     """Базовый класс тренировки."""
     action: int
-    duration_h: float
-    weight_kg: float
+    duration: float
+    weight: float
     M_IN_KM: ClassVar[int] = 1000
     LEN_STEP: ClassVar[float] = 0.65
     MINUTES_IN_HOUR: ClassVar[int] = 60
@@ -35,7 +35,7 @@ class Training:
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        return self.get_distance() / self.duration_h
+        return self.get_distance() / self.duration
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
@@ -44,7 +44,7 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        return InfoMessage(type(self).__name__, self.duration_h,
+        return InfoMessage(type(self).__name__, self.duration,
                            self.get_distance(), self.get_mean_speed(),
                            self.get_spent_calories())
 
@@ -57,40 +57,40 @@ class Running(Training):
 
     def get_spent_calories(self) -> float:
         return ((self.COEFF_CALORIE_RUN1 * self.get_mean_speed()
-                - self.COEFF_CALORIE_RUN2) * self.weight_kg / self.M_IN_KM
-                * self.duration_h * self.MINUTES_IN_HOUR)
+                - self.COEFF_CALORIE_RUN2) * self.weight / self.M_IN_KM
+                * self.duration * self.MINUTES_IN_HOUR)
 
 
 @dataclass
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    height_cm: float
+    height: float
     COEFF_CALORIE_WLK1: ClassVar[float] = 0.035
     COEFF_CALORIE_WLK2: ClassVar[float] = 0.029
 
     def get_spent_calories(self) -> float:
-        return ((self.COEFF_CALORIE_WLK1 * self.weight_kg
-                + (self.get_mean_speed() ** 2 // self.height_cm)
-                * self.COEFF_CALORIE_WLK2 * self.weight_kg)
-                * self.duration_h * self.MINUTES_IN_HOUR)
+        return ((self.COEFF_CALORIE_WLK1 * self.weight
+                + (self.get_mean_speed() ** 2 // self.height)
+                * self.COEFF_CALORIE_WLK2 * self.weight)
+                * self.duration * self.MINUTES_IN_HOUR)
 
 
 @dataclass
 class Swimming(Training):
     """Тренировка: плавание."""
-    length_pool_m: float
+    length_pool: float
     count_pool: float
     LEN_STEP: ClassVar[float] = 1.38
     COEFF_CALORIE_SWM1: ClassVar[float] = 1.1
     COEFF_CALORIE_SWM2: ClassVar[int] = 2
 
     def get_mean_speed(self) -> float:
-        return (self.length_pool_m * self.count_pool / self.M_IN_KM
-                / self.duration_h)
+        return (self.length_pool * self.count_pool / self.M_IN_KM
+                / self.duration)
 
     def get_spent_calories(self) -> float:
         return ((self.get_mean_speed() + self.COEFF_CALORIE_SWM1)
-                * self.COEFF_CALORIE_SWM2 * self.weight_kg)
+                * self.COEFF_CALORIE_SWM2 * self.weight)
 
 
 def read_package(workout_type: str, data: List) -> Training:
